e3bcb107a218f20a6b4a5831cbb304558944ea61
Sonia Lacarra <161872642+Inflanubes@users.noreply.github.com>
2025-03-02 13:03:14 +0000

    Crear Hero.jsx y .css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
-      <main className="text-center p-10">
-        <h1 className="text-4xl font-bold">Bienvenido a AstralPet</h1>
-        <p className="text-lg mt-4">Aquí irá tu landing page...</p>
-      </main>
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import "../styles/hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="overlay"></div>
+      <div className="hero-content">
+        <h1>
+          CARTA ASTRAL <br />
+          <span className="highlight">PERSONALIZADA</span> <br />
+          PARA TU MASCOTA
+        </h1>
+        <p>
+          ¿Alguna vez has sentido que tu mascota y tú tienen un vínculo especial? ¿Te has preguntado por qué actúa de
+          cierta manera o cómo mejorar su bienestar?
+        </p>
+        <p>
+          Con nuestra Carta Astral para Mascotas, descubrirás su personalidad, sus necesidades emocionales y la
+          conexión única que comparte contigo a través de la astrología.
+        </p>
+        <a href="#" className="cta-button">
+          ¡Pídela ya! <span className="cta-arrow">➜</span>
+        </a>
+      </div>
+    </section>
+  );
+}
