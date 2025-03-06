47c18391e9856bb1bb86f1a50d8ba00f2abbe79c
Sonia Lacarra <161872642+Inflanubes@users.noreply.github.com>
2025-03-06 03:13:42 +0000

    Pending changes exported from your codespace

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,24 +23,31 @@ export default function Navbar() {
       <div className="navbar-container">
         {/* Logo y nombre */}
         <div className="logo-container">
-          <img src="https://res.cloudinary.com/dflvexboa/image/upload/v1740994866/AstralPetWeb/uscwnx5kj4o6u5egrbqj.png" alt="Logo" className="logo" />
+          <img 
+            src="https://res.cloudinary.com/dflvexboa/image/upload/v1740994866/AstralPetWeb/uscwnx5kj4o6u5egrbqj.png" 
+            alt="Logo" 
+            className="logo" 
+          />
           <span className="brand-name">ASTRALPET</span>
         </div>
 
-        {/* Menú de navegación alineado a la izquierda */}
+        {/* Menú de navegación */}
         <div className={`menu ${menuOpen ? "mobile-menu open" : "hidden md:flex"}`}>
-          <Link to="home" smooth duration={500} className="menu-item">Home</Link>
-          <Link to="about" smooth duration={500} className="menu-item">Producto</Link>
-          <Link to="team" smooth duration={500} className="menu-item">Sobre nosotros</Link>
-          <Link to="contact" smooth duration={500} className="menu-item">Contacto</Link>
+          <Link to="hero" smooth={true} duration={500} spy={true} hashSpy={true} offset={-70} className="menu-item">Home</Link>
+          <Link to="about-product" smooth={true} duration={500} spy={true} hashSpy={true} offset={-70} className="menu-item">Producto</Link>
+          <Link to="about" smooth={true} duration={500} spy={true} hashSpy={true} offset={-70} className="menu-item">Sobre nosotros</Link>
+          <Link to="security" smooth={true} duration={500} spy={true} hashSpy={true} offset={-70} className="menu-item">Seguridad</Link>
+          <Link to="contact" smooth={true} duration={500} spy={true} hashSpy={true} offset={-70} className="menu-item">Contacto</Link>
         </div>
+
         {/* Botón menú móvil */}
-        <button className="menu-toggle md:hidden"  onClick={() => setMenuOpen(!menuOpen)}>
+        <button className="menu-toggle md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
           ☰
         </button>
       </div>
     </nav>
   );
 }
+
 
 
